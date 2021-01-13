2df33865d60f5625c656963270b49c4454efa290
youngaviator <75797837+youngaviator@users.noreply.github.com>
2021-01-13 17:52:29 +0530

    Add files via upload

---
+++ BaseClass.js
@@ -0,0 +1,26 @@
+class Base {
+    constructor(x, y,width,height,angle) {
+      var options = {
+        'density':1.5,
+        'friction': 1.5,
+        'restitution':0.5
+      };
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      this.image=loadImage("sprites/base.png");
+      World.add(world, this.body);
+    };
+    display(){
+      var pos = this.body.position;
+      var angle = this.body.angle;
+      
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      imageMode(CENTER)
+      image(this.image,0, 0, this.width, this.height);
+      pop();
+    };
+  };
+  
